79b807b9be71b18708bd99098dd952e93aff902a
Tobias Granwald <tobias.granwald@ki.se>
2025-02-03 14:12:30 +0100

    added code used to extract pseudo R2

---
+++ analysis/computational_models/HBI_full2/extract_pseudoR.m
@@ -0,0 +1,33 @@
+clear all
+
+%load data
+fdata = load('all_data_full2.mat');
+
+%load model fit 
+cbm = load('hbi_fitSim_1prior_full2.mat');
+
+params = cbm.cbm.output.parameters{1};
+
+copyfile models/prior1_MS_2invT_F_actBias_mod_HBI.m
+logLike = zeros(length(fdata.data), 1);
+
+for s = 1:length(fdata.data)
+    logLike(s,:) = prior1_MS_2invT_F_actBias_mod_HBI(params(1,:),fdata.data{s});
+end
+
+R_chance = length(fdata.data{1}.act)*log(.5);
+
+pseudoR2 = zeros(length(fdata.data), 1);
+
+for l = 1:length(logLike)
+    pseudoR2(l,:) = 1-(logLike(l,1)/R_chance);
+end
+
+%quantile(logLike, .25)
+%quantile(logLike, .5)
+%quantile(logLike, .75)
+
+% reported in the manuscript
+quantile(pseudoR2, .25)
+quantile(pseudoR2, .5)
+quantile(pseudoR2, .75)
